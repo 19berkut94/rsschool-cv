faed983a8e7d7fa95f33b0a1e1bc03b8e1015c3e
Ilya Bohater <19berkut94@gmail.com>
2020-11-23 16:11:35 +0300

    fix: group code in functions

--- main.js
+++ main.js
@@ -1,105 +1,126 @@
 "use strict";
 
-let operators = document.querySelectorAll(".operator"),
-  numbers = document.querySelectorAll(".number"),
-  display = document.querySelector(".display"),
-  clearButtons = document.querySelectorAll(".cancel"),
-  result = document.querySelector("#result"),
-  point = document.querySelector("#point"),
-  a = "",
-  b = "",
-  c = "",
-  memoryOperator = "";
+let operators = document.querySelectorAll(".operator");
+let numbers = document.querySelectorAll(".number");
+let display = document.querySelector(".display");
+let clearButtons = document.querySelectorAll(".cancel");
+let result = document.querySelector("#result");
+let point = document.querySelector("#point");
+let operandFirst = "";
+let operandSecond = "";
+let operandsAnswer = "";
+let memoryOperator = "";
 
-for (let i = 0; i < clearButtons.length; i++) {
-  let cancelBtn = clearButtons[i];
-  cancelBtn.addEventListener("click", function (e) {
-    clear(e.target.textContent);
-  });
+makeCalculatorWork();
+
+function makeCalculatorWork() {
+  makeNumbersPush();
+  makeOperatorsOperate();
+  makeClearButtonsClean();
+  makeDecimalUse()
 }
 
-for (let i = 0; i < numbers.length; i++) {
-  let num = numbers[i];
-  num.addEventListener("click", function (e) {
-    if (c !== "") {
-      c = "";
-      display.value = "0";
-      clickNumbers(e.target.textContent);
-    } else if (c === "") {
-      clickNumbers(e.target.textContent);
+function makeNumbersPush() {
+  function clickNumbers(number) {
+    if (display.value === "0") {
+      display.value = number;
+    } else {
+      display.value += number;
     }
-  });
+  }
+
+  for (let i = 0; i < numbers.length; i++) {
+    let num = numbers[i];
+    num.addEventListener("click", function (e) {
+      if (operandsAnswer !== "") {
+        operandsAnswer = "";
+        display.value = "0";
+        clickNumbers(e.target.textContent);
+      } else if (operandsAnswer === "") {
+        clickNumbers(e.target.textContent);
+      }
+    });
+  }
 }
 
-for (let i = 0; i < operators.length; i++) {
-  let operator = operators[i];
+function makeOperatorsOperate() {
+  function clickOperators(op) {
+    if (operandFirst === "") {
+      memoryOperator = op;
+      operandFirst = Number(display.value);
+      display.value = "";
+    } else {
+      if (operandFirst !== "" && Number(display.value)) {
+        operandSecond = display.value;
+        operandsAnswer = eval(
+          `${operandFirst}${memoryOperator} ${operandSecond}`
+        );
+        display.value = operandsAnswer;
+        memoryOperator = op;
+        operandFirst = "";
+        if (operandSecond !== "" && operandsAnswer !== "") {
+          operandFirst = operandsAnswer;
+        }
+        //vopros----------------------------------
+      }
+    }
+  }
 
-  operator.addEventListener("click", function (e) {
-    clickOperators(e.target.textContent);
-  });
+  for (let i = 0; i < operators.length; i++) {
+    let operator = operators[i];
+  
+    operator.addEventListener("click", function (e) {
+      clickOperators(e.target.textContent);
+    });
+  
+    result.onclick = function () {
+      if (operandFirst !== "") {
+        operandSecond = Number(display.value);
+        operandsAnswer = eval(
+          `${operandFirst}${memoryOperator} ${operandSecond}`
+        );
+        display.value = operandsAnswer;
+        operandFirst = "";
+        operandSecond = "";
+      }
+    };
+  }
+}
 
-  result.onclick = function () {
-    if (a !== "") {
-      b = Number(display.value);
-      c = eval(`${a} ${memoryOperator} ${b}`);
-      display.value = c;
-      a = "";
-      b = "";
+function makeDecimalUse() {
+  point.onclick = function () {
+    if (display.value === "0" || display.value === "") {
+      display.value = "0.";
+    } else if (!display.value.includes(".")) {
+      display.value += point.innerText;
+      if (operandsAnswer !== "") {
+        display.value = "0.";
+        operandsAnswer = "";
+      }
+    } else {
+      if (operandsAnswer !== "" && String(display.value).includes(".")) {
+        display.value = "0.";
+        operandsAnswer = "";
+      }
     }
   };
 }
 
-function clickNumbers(number) {
-  if (display.value === "0") {
-    display.value = number;
-  } else {
-    display.value += number;
-  }
-}
-
-function clickOperators(op) {
-  if (a === "") {
-    memoryOperator = op;
-    a = Number(display.value);
-    display.value = "";
-  } else {
-    if (a !== "" && Number(display.value)) {
-      b = display.value;
-      c = eval(`${a} ${memoryOperator} ${b}`);
-      display.value = c;
-      memoryOperator = op;
-      a = "";
-      if (b !== "" && c !== "") {
-        a = c;
-      }
-      //vopros----------------------------------
+function makeClearButtonsClean() {
+  function clear(cancelButton) {
+    if (cancelButton === "ce") {
+      display.value = "0";
+    } else if (cancelButton === "c") {
+      display.value = "0";
+      operandFirst = "";
+      operandSecond = "";
     }
   }
-}
 
-function clear(cancelBtn) {
-  if (cancelBtn === "ce") {
-    display.value = "0";
-  } else if (cancelBtn === "c") {
-    display.value = "0";
-    a = "";
-    b = "";
+  for (let i = 0; i < clearButtons.length; i++) {
+    let cancelButton = clearButtons[i];
+    cancelButton.addEventListener("click", function (e) {
+      clear(e.target.textContent);
+    });
   }
 }
-
-point.onclick = function () {
-  if (display.value === "0" || display.value === "") {
-    display.value = "0.";
-  } else if (!display.value.includes(".")) {
-    display.value += point.innerText;
-    if (c !== "") {
-      display.value = "0.";
-      c = "";
-    }
-  } else {
-    if (c !== "" && String(display.value).includes(".")) {
-      display.value = "0.";
-      c = "";
-    }
-  }
-};
