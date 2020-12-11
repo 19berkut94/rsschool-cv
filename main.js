5200814048a75ec41f53e1878e07b73d270ef180
Ilya Bohater <19berkut94@gmail.com>
2020-12-12 01:31:32 +0300

    feat: add js

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 'use strict';
 
+const settings = document.querySelectorAll('.work-area input')
+
+initializeVars()
+
+function initializeVars() {
+    addEventHandler('change', settings, changeSettings)
+    addEventHandler('mousemove', settings, changeSettings)
+}
+
+function changeSettings() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+function addEventHandler(event, arr, callback) {
+    arr.forEach(el => {
+        el.addEventListener(event, callback)
+    })
+}
