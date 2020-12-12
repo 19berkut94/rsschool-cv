e22d415d3c954b8747f03792e79772ae13a27f67
Ilya Bohater <19berkut94@gmail.com>
2020-12-12 22:02:37 +0300

    feat: add js

--- gal.js
+++ gal.js
@@ -1,2 +1,24 @@
-'use strict';
+"use strict";
 
+const slides = document.querySelectorAll(".container__item");
+
+function chooseSlide() {
+  slides.forEach((slide) => {
+    slide.classList.remove("open");
+  });
+  this.classList.add("open");
+}
+
+function toggleShowedText(e) {
+  if (e.propertyName.includes("flex-grow")) {
+    this.classList.toggle("showed-text");
+  }
+}
+
+slides.forEach((slide) => {
+  slide.addEventListener("click", chooseSlide);
+});
+
+slides.forEach((slide) => {
+  slide.addEventListener("transitionend", toggleShowedText);
+});
