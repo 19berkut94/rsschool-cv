eca5fa397b604ee556e3737ca521be14cc2c2a75
Ilya Bohater <19berkut94@gmail.com>
2020-12-29 01:47:29 +0300

    fix: correct root values and event type

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ const settings = document.querySelectorAll('.work-area input')
 initializeVars()
 
 function initializeVars() {
-    addEventHandler('change', settings, changeSettings)
-    addEventHandler('mousemove', settings, changeSettings)
+    // addEventHandler('mousemove', settings, changeSettings)
+    // addEventHandler('change', settings, changeSettings)
+    settings.forEach(set => set.addEventListener('change', changeSettings))
+    settings.forEach(set => set.addEventListener('input', changeSettings))
 }
 
 function changeSettings() {
@@ -14,8 +16,8 @@ function changeSettings() {
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
-function addEventHandler(event, arr, callback) {
-    arr.forEach(el => {
-        el.addEventListener(event, callback)
-    })
-}
+// function addEventHandler(event, arr, callback) {
+//     arr.forEach(el => {
+//         el.addEventListener(event, callback)
+//     })
+// }
